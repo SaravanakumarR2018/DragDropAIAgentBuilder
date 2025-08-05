1cef7006bacb050acd8f0301d5bb4370fbc31e97
Bharani0012 <bharanitharan695@gmail.com>
2025-08-05 12:28:31 +0530

    modified constant.ts

--- src/frontend/src/constants/constants.ts
+++ src/frontend/src/constants/constants.ts
@@ -1027,7 +1027,7 @@ export const POLLING_MESSAGES = {
 export const BUILD_POLLING_INTERVAL = 25;
 
 export const IS_AUTO_LOGIN =
-  import.meta.env.VITE_AUTO_LOGIN !== "false";
+  (import.meta.env.VITE_AUTO_LOGIN).toLowerCase() !== "false";
 
 export const AUTO_LOGIN_RETRY_DELAY = 2000;
 export const AUTO_LOGIN_MAX_RETRY_DELAY = 60000;
