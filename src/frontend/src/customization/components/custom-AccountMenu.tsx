3195995f0bd290771324af04e4715ad64063aed8
unknown <kabik5095@gmail.com>
2025-09-03 17:48:52 +0530

    fix the signout

--- src/frontend/src/customization/components/custom-AccountMenu.tsx
+++ src/frontend/src/customization/components/custom-AccountMenu.tsx
@@ -1,7 +1,28 @@
+import { IS_CLERK_AUTH, useLogout } from "@/clerk/auth";
 import { AccountMenu } from "@/components/core/appHeaderComponent/components/AccountMenu";
+import { UserButton} from "@clerk/clerk-react";
+import { LogOut } from "lucide-react";
 
 export function CustomAccountMenu() {
-  return <AccountMenu />;
+  const { mutateAsync: logout } = useLogout();
+
+  if (!IS_CLERK_AUTH) return <AccountMenu />;
+
+  return (
+    <div className="h-6 w-6">
+      <UserButton appearance={{ elements: { avatarBox: "h-6 w-6", userButtonPopoverActionButton__signOut: "hidden" } }}>
+        <UserButton.MenuItems>
+          <UserButton.Action
+            label="logout"
+            labelIcon={<LogOut className="w-4 h-4" />}
+            onClick={ async () => {
+              await logout();
+            }}
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+    </div>
+  );
 }
 
 export default CustomAccountMenu;
