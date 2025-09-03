ace4324014bf055310d48dca0555ab7254cc2800
unknown <kabik5095@gmail.com>
2025-09-03 21:45:48 +0530

    Userprofile was added in clerk

--- src/frontend/src/customization/components/custom-AccountMenu.tsx
+++ src/frontend/src/customization/components/custom-AccountMenu.tsx
@@ -1,7 +1,26 @@
+import { IS_CLERK_AUTH, useLogout } from "@/clerk/auth";
 import { AccountMenu } from "@/components/core/appHeaderComponent/components/AccountMenu";
+import { UserButton} from "@clerk/clerk-react";
+import { LogOut } from "lucide-react";
 
 export function CustomAccountMenu() {
-  return <AccountMenu />;
+  const { mutateAsync: logout } = useLogout();
+
+  if (!IS_CLERK_AUTH) return <AccountMenu />;
+
+  return (
+    <div className="h-6 w-6">
+      <UserButton appearance={{ elements: { avatarBox: "h-6 w-6", userButtonPopoverActionButton__signOut: "hidden" } }}>
+        <UserButton.MenuItems>
+          <UserButton.Action
+            label="Sign out"
+            labelIcon={<LogOut className="w-4 h-4" />}
+            onClick={logout}
+          />
+        </UserButton.MenuItems>
+      </UserButton>
+    </div>
+  );
 }
 
 export default CustomAccountMenu;
