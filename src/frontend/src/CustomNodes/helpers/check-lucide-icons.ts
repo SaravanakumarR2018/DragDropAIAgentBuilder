9372d87120b195bc2d9cde793a054cff63cdf232
unknown <kabik5095@gmail.com>
2025-09-24 12:34:14 +0530

    fix the icon bundle

--- src/frontend/src/CustomNodes/helpers/check-lucide-icons.ts
+++ src/frontend/src/CustomNodes/helpers/check-lucide-icons.ts
@@ -1,10 +1,9 @@
 import { categoryIcons } from "@/utils/styleUtils";
-import * as lucideIcons from "lucide-react";
+
 import dynamicIconImports from "lucide-react/dynamicIconImports";
 
 export const checkLucideIcons = (iconName: string): boolean => {
   return (
-    !!lucideIcons[iconName] ||
     !!dynamicIconImports[iconName] ||
     !!categoryIcons[iconName]
   );
