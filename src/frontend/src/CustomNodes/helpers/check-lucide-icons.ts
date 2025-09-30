62f2eba6c7c44612273ed06a8402845259337dc2
unknown <kabik5095@gmail.com>
2025-09-30 13:52:11 +0530

    const files fix

--- src/frontend/src/CustomNodes/helpers/check-lucide-icons.ts
+++ src/frontend/src/CustomNodes/helpers/check-lucide-icons.ts
@@ -4,7 +4,7 @@ const dynamicIconImportsPromise = import("lucide-react/dynamicIconImports").then
   (mod) => mod.default,
 );
 
-  export const checkLucideIcons = async (iconName: string): Promise<boolean> => {
+export const checkLucideIcons = async (iconName: string): Promise<boolean> => {
   const [lucideIcons, dynamicIconImports] = await Promise.all([
     lucideIconsPromise,
     dynamicIconImportsPromise,
