8794a41a747f02af776438bcc83b5968e5690482
unknown <kabik5095@gmail.com>
2025-10-02 10:26:59 +0530

    solve the unused line issue in react-icons

--- src/frontend/src/CustomNodes/helpers/check-lucide-icons.ts
+++ src/frontend/src/CustomNodes/helpers/check-lucide-icons.ts
@@ -1,15 +1,9 @@
 import { categoryIcons } from "@/utils/styleUtils";
-const lucideIconsPromise = import("lucide-react");
-const dynamicIconImportsPromise = import("lucide-react/dynamicIconImports").then(
-  (mod) => mod.default,
-);
+const lucideIconsPromise=import("lucide-react");
+const dynamicIconImportsPromise = import("lucide-react/dynamicIconImports").then((mod)=>mod.default);
 
-export const checkLucideIcons = async (iconName: string): Promise<boolean> => {
-  const [lucideIcons, dynamicIconImports] = await Promise.all([
-    lucideIconsPromise,
-    dynamicIconImportsPromise,
-  ]);
-
+export const checkLucideIcons =async (iconName: string):Promise<boolean> => {
+  const [lucideIcons, dynamicIconImports] = await Promise.all([lucideIconsPromise, dynamicIconImportsPromise]);
   return (
     !!lucideIcons[iconName] ||
     !!dynamicIconImports[iconName] ||
