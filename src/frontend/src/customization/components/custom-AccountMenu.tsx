39aa4cff75d9858a35183e34417c57a9a006ed97
Bharani0012 <bharanitharan695@gmail.com>
2025-11-06 21:31:38 +0530

    removed frontend and modified nginx.conf

--- src/frontend/src/customization/components/custom-AccountMenu.tsx
+++
@@ -1,7 +0,0 @@
-import { ClerkAccountMenu as AccountMenu } from "./ClerkAccountMenu"
-
-export function CustomAccountMenu() {
-  return <AccountMenu />;
-}
-
-export default CustomAccountMenu;
--- src/frontend/src/utils/lazyLoadAce.ts
+++
@@ -1,6 +0,0 @@
-export async function lazyLoadAce() {
-  await import("ace-builds/src-noconflict/ext-language_tools");
-  await import("ace-builds/src-noconflict/mode-python");
-  await import("ace-builds/src-noconflict/theme-github");
-  await import("ace-builds/src-noconflict/theme-twilight");
-}
