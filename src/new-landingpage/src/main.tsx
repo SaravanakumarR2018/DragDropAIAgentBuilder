4819e37a032fab9c68831f64daa77d579b23d693
Saravana Kumar Rajendran <srajendran@microsoft.com>
2025-11-19 07:52:53 +0530

    feat: add login route to landing page

--- src/new-landingpage/src/main.tsx
+++ src/new-landingpage/src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AppWithProvider from "@/clerk/auth";
 import App from "./App";
 import "./index.css";
+import "@/style/classes.css";
+import "@/style/index.css";
+import "@/App.css";
+import "@/style/applies.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppWithProvider>
+      <BrowserRouter basename="/new/landingpage">
+        <App />
+      </BrowserRouter>
+    </AppWithProvider>
   </React.StrictMode>
 );
--- src/new-landingpage/vite.config.ts
+++ src/new-landingpage/vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr(), tsconfigPaths()],
   base: "/new/landingpage/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "../frontend/src"),
+    },
+  },
   build: {
     outDir: "dist",
     emptyOutDir: true
