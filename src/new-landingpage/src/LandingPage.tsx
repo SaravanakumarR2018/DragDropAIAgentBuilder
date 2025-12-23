a6490815c3eab432d7cacea0e265d24c4786fc60
Bharani0012 <bharanitharan695@gmail.com>
2025-12-23 15:41:12 +0530

    updated import path

--- src/new-landingpage/src/LandingPage.tsx
+++ src/new-landingpage/src/LandingPage.tsx
@@ -5,7 +5,7 @@ import { useAuth } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 import VisualWorkflow from "./new-assets/VisualWorkflow.webp";
 import demoWalkthrough from "./new-assets/demo-walkthrough.webp";
-import logoicon from "./new-assets/visualailogo.webp?inline";
+import logoicon from "./new-assets/visualailogo.webp";
 import {
   clearStoredOrgSelection,
   LANGFLOW_ACCESS_TOKEN,
--- src/new-landingpage/src/main.tsx
+++ src/new-landingpage/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { CookiesProvider } from "react-cookie";
-import faviconUrl from "./new-assets/visualailogo.webp?inline";
+import faviconUrl from "./new-assets/visualailogo.webp";
 import App from "./App";
 import "./index.css";
 
