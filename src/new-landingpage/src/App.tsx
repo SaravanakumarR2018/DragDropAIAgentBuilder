5811e9b1aed0580cb75fb95f539fb27aa8e73390
Saravana Kumar Rajendran <saravana.k.r@gmail.com>
2026-01-02 14:50:40 +0530

    Keep landing page at root even when signed in

--- src/new-landingpage/src/App.tsx
+++ src/new-landingpage/src/App.tsx
@@ -1,4 +1,3 @@
-import { useAuth } from "@clerk/clerk-react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import type { PropsWithChildren } from "react";
 import { LANDING_BASENAME } from "./landingRoutes";
@@ -32,37 +31,13 @@ function FlowsRedirect() {
   return null;
 }
 
-function RootRoute() {
-  const { isSignedIn } = useAuth();
-  const [cookies] = useCookies([LANGFLOW_ACCESS_TOKEN, LANGFLOW_REFRESH_TOKEN]);
-
-  console.log("[App] RootRoute render", { isSignedIn });
-
-  if (isSignedIn) {
-    const workspaceReady = hasWorkspaceSession(cookies);
-    const destination = workspaceReady ? "/flows" : "/organization";
-
-    console.log("[App] Redirecting signed-in user from root", {
-      workspaceReady,
-      destination,
-    });
-
-    if (workspaceReady) {
-      return <FlowsRedirect />;
-    }
-    return <Navigate to={destination} replace />;
-  }
-
-  return <LandingPage />;
-}
-
 export default function App() {
   console.log(`[App] App mounted with basename ${LANDING_BASENAME}`);
 
   return (
     <BrowserRouter basename={LANDING_BASENAME}>
       <Routes>
-        <Route path="/" element={<RootRoute />} />
+        <Route path="/" element={<LandingPage />} />
         <Route
           path="/login"
           element={
