23c4d196fa7fb96a44c61ba8c48fda98eeff6f21
Bharani0012 <bharanitharan695@gmail.com>
2026-05-11 14:48:57 +0530

    Escape variables in NGINX configuration for proper proxy header handling

--- deploy/ebs-staging-prod.sh
+++ deploy/ebs-staging-prod.sh
@@ -467,10 +467,10 @@ server {
     chunked_transfer_encoding off;
 
     proxy_set_header Connection '';
-    proxy_set_header Host $host;
-    proxy_set_header X-Forwarded-Proto $scheme;
-    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header Host \$host;
+    proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header X-Real-IP \$remote_addr;
 
     # Long-lived stream
     proxy_read_timeout 86400;
